01a62fe89b73c7e8a082b36cf40e4489c91e6b3a
Stefanneedhelp <stefan.needhelp2025@gmail.com>
2025-04-20 20:41:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot je startovao!")
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(f"✅ Tvoj CHAT_ID je: {chat_id}")
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("✅ Bot je pokrenut. Čeka komande...")
     app.run_polling()
