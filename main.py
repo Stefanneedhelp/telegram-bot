5b8f0dea12b3255ca4bd0c340442c17b26507833
Stefanneedhelp <stefan.needhelp2025@gmail.com>
2025-04-20 23:03:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,5 +65,12 @@ async def test():
     await send_telegram_message("✅ Ovo je test poruka od bota!")
 
 asyncio.run(test())
+import asyncio from telegram import Bot import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN") CHAT_ID = 1601980040 # tvoj chat_id
+
+async def send_test(): bot = Bot(token=BOT_TOKEN) await bot.send_message(chat_id=CHAT_ID, text="✅ Test poruka od bota!")
+
+asyncio.run(send_test())
 
 
