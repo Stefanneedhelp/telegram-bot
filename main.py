cd1275e4df8623f9ac79c111780e3ef32a210812
Stefan Dordevic <stefan.needhelp2025@gmail.com>
2025-04-21 11:24:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CHAT_ID = os.getenv("CHAT_ID")
 INTERESANTNA_ZANIMANJA = [
     "Montage de meubles", "Menuisier", "ébéniste", "Électricité", "Pose carrelage",
     "Percer", "fixer", "Enduit", "Pose de porte", "portail", "Découpe",
-    "Pose sanitaire", "Pose parquet", "Peinture","Poser","Installation","Reparer","Réparer"
+    "Pose sanitaire", "Pose parquet", "Peinture","Poser","Installation","Reparer","Réparer","Revêtements de sol" 
 ]
 
 URL = "https://www.needhelp.com/trouver-un-job"
